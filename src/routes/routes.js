22571d250904c051ba4240020dc3ac26dfb6e525
Tyrel Chambers <tychambers3@gmail.com>
2020-09-22 20:47:35 -0400

    add routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,1 +1,6 @@
 export const HOME = "/";
+export const FUNCTIONS = "/functions";
+export const DECIMALS = "/decimals";
+export const ADDITION = "/addition";
+export const SUBTRACTION = "/subtraction";
+export const PERCENTAGES = "/percentages";
